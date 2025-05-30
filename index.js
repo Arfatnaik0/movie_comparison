b033bbc690a1eae8729652636bc02edf7655bf6b
Arfatnaik0 <arfatnaik05@gmail.com>
2025-05-30 07:09:37 +0000

    2nd

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetchData=async(searchTerm)=>{
+    const response =await axios.get('http://www.omdbapi.com/', {
+        params:{
+            apikey:'1498ade9',
+            s:searchTerm
+        }
+    });
+
+    return response.data.Search;
+};
+
+const input = document.querySelector('input');
+
+const onInput =async event=>{
+   const movies=await fetchData(event.target.value);
+   console.log(movies);
+};
+
+input.addEventListener('input',debounce(onInput,800));
